fbd542d8149882ab4b58890455b6dd5038d0e96b
Igor Mota <igormello399@gmail.com>
2026-04-02 18:21:38 -0300

    movidos metodos SortearPalavraSecreta e CompararPalavras para classe Jogo

--- Termo.ConsoleApp/Jogo.cs
+++ Termo.ConsoleApp/Jogo.cs
@@ -1,8 +1,105 @@
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+
 namespace Termo.ConsoleApp;
 
-class Jogo
+static class Jogo
 {
+    static string palavraSecreta = "";
+    private static void SortearPalavraSecreta()
+    {
+        string[] palavras =
+           {
+            "AFOIS",
+            "BIRRA",
+            "CABRA",
+            "DADOS",
+            "ECHOS",
+            "FAROL",
+            "GOLPE",
+            "HORAS",
+            "IDEAL",
+            "JOGOS",
+            "KILOS",
+            "LACOS",
+            "MAGOS",
+            "NAVES",
+            "OLHOS",
+            "PILOS",
+            "QUINA",
+            "RACAS",
+            "SALAS",
+            "TUFOS"
+        };
 
+        int indiceAleatorio = RandomNumberGenerator.GetInt32(palavras.Length);
+        palavraSecreta = palavras[indiceAleatorio];
+    }
+
+    public static void CompararPalavras()
+    {
+        SortearPalavraSecreta();
+        int tentativas = 5;
+        while (tentativas != 0)
+        {
+            System.Console.WriteLine($"Tentativas: {tentativas}");
+            System.Console.Write(">");
+            string? palavraChutada = Console.ReadLine()?.ToUpper();
+
+            if (string.IsNullOrEmpty(palavraChutada) || palavraChutada?.Length != 5 || !Regex.IsMatch(palavraChutada ?? "", @"^[A-Z]+$"))
+            {
+                System.Console.WriteLine("Digite uma palavra valida de 5 letras(apenas letras).Pressione ENTER para tentar novamente");
+                Console.ReadLine();
+                continue;
+            }
+
+            for (int indiceLetra = 0; indiceLetra < palavraChutada.Length; indiceLetra++)
+            {
+                if (palavraChutada[indiceLetra] == palavraSecreta[indiceLetra])
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+                    System.Console.Write(palavraChutada[indiceLetra]);
+
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+                else if (palavraSecreta.Contains(palavraChutada[indiceLetra]))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+                    System.Console.Write(palavraChutada[indiceLetra]);
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                    System.Console.Write(palavraChutada[indiceLetra]);
+
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+            }
+
+            Console.ReadLine();
+
+            if (palavraChutada == palavraSecreta)
+            {
+                System.Console.WriteLine("Voce acertou!");
+                break;
+            }
+            else
+            {
+                tentativas--;
+                continue;
+            }
+        }
+
+        System.Console.WriteLine($"Voce perdeu! A palavra era: {palavraSecreta} ");
+    }
 }
 
 
--- Termo.ConsoleApp/Program.cs
+++ Termo.ConsoleApp/Program.cs
@@ -8,11 +8,9 @@ class Program
     static void Main(string[] args)
     {
 
-        Console.Clear();
+        //Console.Clear();
         ExibirCabecalho();
-        string palavraSecreta = SortearPalavraSecreta();
-        CompararPalavras(palavraSecreta);
-
+        Jogo.CompararPalavras();
 
     }
 
@@ -24,100 +22,9 @@ class Program
 
     }
 
-    static string SortearPalavraSecreta()
-    {
-        string[] palavras =
-           {
-            "AFOIS",
-            "BIRRA",
-            "CABRA",
-            "DADOS",
-            "ECHOS",
-            "FAROL",
-            "GOLPE",
-            "HORAS",
-            "IDEAL",
-            "JOGOS",
-            "KILOS",
-            "LACOS",
-            "MAGOS",
-            "NAVES",
-            "OLHOS",
-            "PILOS",
-            "QUINA",
-            "RACAS",
-            "SALAS",
-            "TUFOS"
-        };
-
-        int indiceAleatorio = RandomNumberGenerator.GetInt32(palavras.Length);
 
 
-        return palavras[indiceAleatorio];
-    }
 
-    static void CompararPalavras(string palavraSecreta)
-    {
-        int tentativas = 5;
-        while (tentativas != 0)
-        {
-            System.Console.WriteLine($"Tentativas: {tentativas}");
-            System.Console.Write(">");
-            string? palavraChutada = Console.ReadLine()?.ToUpper();
-
-            if (string.IsNullOrEmpty(palavraChutada) || palavraChutada?.Length != 5 || !Regex.IsMatch(palavraChutada ?? "", @"^[A-Z]+$"))
-            {
-                System.Console.WriteLine("Digite uma palavra valida de 5 letras(apenas letras).Pressione ENTER para tentar novamente");
-                Console.ReadLine();
-                continue;
-            }
-
-            for (int indiceLetra = 0; indiceLetra < palavraChutada.Length; indiceLetra++)
-            {
-                if (palavraChutada[indiceLetra] == palavraSecreta[indiceLetra])
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-
-                    System.Console.Write(palavraChutada[indiceLetra]);
-
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                }
-                else if (palavraSecreta.Contains(palavraChutada[indiceLetra]))
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-
-                    System.Console.Write(palavraChutada[indiceLetra]);
-
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-
-                    System.Console.Write(palavraChutada[indiceLetra]);
-
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                }
-            }
-
-            Console.ReadLine();
-
-            if (palavraChutada == palavraSecreta)
-            {
-                System.Console.WriteLine("Voce acertou!");
-                break;
-            }
-            else
-            {
-                tentativas--;
-                continue;
-            }
-        }
-
-        System.Console.WriteLine($"Voce perdeu! A palavra era: {palavraSecreta} ");
-    }
 }
 
 
