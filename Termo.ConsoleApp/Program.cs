e566d7cf2c3124c7f498984228fd2ef076975f6f
Igor Mota <igormello399@gmail.com>
2026-04-02 17:55:04 -0300

    Adicionado metodo CompararPalavras

--- Termo.ConsoleApp/Program.cs
+++ Termo.ConsoleApp/Program.cs
@@ -5,77 +5,15 @@ namespace Termo.ConsoleApp;
 
 class Program
 {
-
     static void Main(string[] args)
     {
 
-        int tentativas = 5;
+        Console.Clear();
+        ExibirCabecalho();
+        string palavraSecreta = SortearPalavraSecreta();
+        CompararPalavras(palavraSecreta);
 
 
-        while (tentativas != 0)
-        {
-            Console.Clear();
-            ExibirCabecalho();
-            string palavraSecreta = SortearPalavraSecreta();
-
-            //logica
-
-            System.Console.WriteLine($"Tentativas: {tentativas}");
-            System.Console.Write(">");
-            string? palavraChutada = Console.ReadLine()?.ToUpper();
-
-            if (string.IsNullOrEmpty(palavraChutada) || palavraChutada?.Length != 5 || !Regex.IsMatch(palavraChutada ?? "", @"^[A-Z]+$"))
-            {
-                System.Console.WriteLine("Digite uma palavra valida de 5 letras(apenas letras).Pressione ENTER para tentar novamente");
-                Console.ReadLine();
-                continue;
-            }
-
-            for (int indiceLetra = 0; indiceLetra < palavraChutada.Length; indiceLetra++)
-            {
-                if (palavraChutada[indiceLetra] == palavraSecreta[indiceLetra])
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    System.Console.Write(palavraChutada[indiceLetra]);
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                }
-                else if (palavraSecreta.Contains(palavraChutada[indiceLetra]))
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-
-                    System.Console.Write(palavraChutada[indiceLetra]);
-
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-
-                    System.Console.Write(palavraChutada[indiceLetra]);
-
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                }
-
-            }
-
-            Console.ReadLine();
-
-            if (palavraChutada == palavraSecreta)
-            {
-                System.Console.WriteLine("Voce acertou!");
-                break;
-            }
-            else
-            {
-                tentativas--;
-                continue;
-            }
-
-
-        }
-        System.Console.WriteLine("Voce perdeu! ");
     }
 
     static void ExibirCabecalho()
@@ -118,4 +56,68 @@ class Program
         return palavras[indiceAleatorio];
     }
 
+    static void CompararPalavras(string palavraSecreta)
+    {
+        int tentativas = 5;
+        while (tentativas != 0)
+        {
+            System.Console.WriteLine($"Tentativas: {tentativas}");
+            System.Console.Write(">");
+            string? palavraChutada = Console.ReadLine()?.ToUpper();
+
+            if (string.IsNullOrEmpty(palavraChutada) || palavraChutada?.Length != 5 || !Regex.IsMatch(palavraChutada ?? "", @"^[A-Z]+$"))
+            {
+                System.Console.WriteLine("Digite uma palavra valida de 5 letras(apenas letras).Pressione ENTER para tentar novamente");
+                Console.ReadLine();
+                continue;
+            }
+
+            for (int indiceLetra = 0; indiceLetra < palavraChutada.Length; indiceLetra++)
+            {
+                if (palavraChutada[indiceLetra] == palavraSecreta[indiceLetra])
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+                    System.Console.Write(palavraChutada[indiceLetra]);
+
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+                else if (palavraSecreta.Contains(palavraChutada[indiceLetra]))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+                    System.Console.Write(palavraChutada[indiceLetra]);
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                    System.Console.Write(palavraChutada[indiceLetra]);
+
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+            }
+
+            Console.ReadLine();
+
+            if (palavraChutada == palavraSecreta)
+            {
+                System.Console.WriteLine("Voce acertou!");
+                break;
+            }
+            else
+            {
+                tentativas--;
+                continue;
+            }
+        }
+
+        System.Console.WriteLine($"Voce perdeu! A palavra era: {palavraSecreta} ");
+    }
 }
+
+
